c8d3cfaff26894d572f8746a332a0285648a7b2e
Andre0y <andreysirotkin2009@gmail.com>
2023-11-05 23:30:32 +0300

    added Random multi-spawn

--- Assets/Scripts/Tower/TowerBuilder.cs
+++ Assets/Scripts/Tower/TowerBuilder.cs
@@ -1,36 +1,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using PathCreation;
-using System;
 
 public class TowerBuilder : MonoBehaviour
 {
     [SerializeField] private PathCreator _pathCreator;
     [SerializeField] private Human _human;
-    [SerializeField] private int _humansInTower;
-    [SerializeField] private int _towersOnRoad;
 
     private Vector3 _spawnPosition;
     private Vector3 _startPathPosition;
     private Vector3 _finishPathPosition;
+
+    private int _humansInTower;
+    private int _towersOnRoad;
+    
     private float _spawnPositionYMultiplier = 1.5f;
-    private float _distanseBetweenTowers;
+    private float _distanñeBetweenTowers;
 
     private void Awake()
     {
-        _startPathPosition = _pathCreator.path.GetPointAtDistance(0);
-        _finishPathPosition = _pathCreator.path.GetDirectionAtDistance(1);
+        _towersOnRoad = Random.Range(1, 10);
         
-        _distanseBetweenTowers = (_finishPathPosition.x - _startPathPosition.x) / _towersOnRoad;
-        _spawnPosition = _pathCreator.path.GetPointAtDistance(_distanseBetweenTowers);
+        GetPreviousPositionsAndCalculations();
     }
 
     public List<Human> Build()
     {
         List<Human> humans = new List<Human>();
-       
+
         for (int i = 0; i < _towersOnRoad; i++)
         {
+            _humansInTower = Random.Range(1, 10);
+
             for (int j = 0; j < _humansInTower; j++)
             {
                 for (int k = 0; k < _humansInTower; k++)
@@ -45,7 +46,7 @@ public class TowerBuilder : MonoBehaviour
                 _spawnPosition = new Vector3(_spawnPosition.x, _spawnPosition.y + _human.transform.localScale.y * _spawnPositionYMultiplier, _spawnPosition.z);
             }
 
-            _spawnPosition = new Vector3(_spawnPosition.x + _distanseBetweenTowers, 0, _spawnPosition.z);
+            _spawnPosition = new Vector3(_spawnPosition.x + _distanñeBetweenTowers, 0, _spawnPosition.z);
         }
         
         return humans;
@@ -56,5 +57,14 @@ public class TowerBuilder : MonoBehaviour
         Human newHuman = Instantiate(_human, _spawnPosition, Quaternion.Euler(0, -90, 0), transform);
         
         return newHuman;
+    }
+
+    private void GetPreviousPositionsAndCalculations()
+    {
+        _startPathPosition = _pathCreator.path.GetPointAtDistance(0);
+        _finishPathPosition = _pathCreator.path.GetDirectionAtDistance(1);
+
+        _distanñeBetweenTowers = (_finishPathPosition.x - _startPathPosition.x) / _towersOnRoad;
+        _spawnPosition = _pathCreator.path.GetPointAtDistance(_distanñeBetweenTowers);
     }
 }
