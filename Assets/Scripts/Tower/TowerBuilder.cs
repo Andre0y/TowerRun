8da0c8d223c9e873fbbef7e87d62e89c9b72ba6b
Andre0y <andreysirotkin2009@gmail.com>
2023-11-05 10:08:06 +0300

    fixed multi-spawn for towers

--- Assets/Scripts/Tower/TowerBuilder.cs
+++ Assets/Scripts/Tower/TowerBuilder.cs
@@ -5,10 +5,12 @@ using System;
 
 public class TowerBuilder : MonoBehaviour
 {
+    [SerializeField] private PathCreator _pathCreator;
+    [SerializeField] private Transform _startPosition;
+    [SerializeField] private Transform _finishPosition;
     [SerializeField] private Human _human;
     [SerializeField] private int _humansInTower;
     [SerializeField] private int _towersOnRoad;
-    [SerializeField] private PathCreator _pathCreator;
 
     private Vector3 _spawnPosition;
     private float _spawnPositionYMultiplier = 1.5f;
@@ -16,9 +18,9 @@ public class TowerBuilder : MonoBehaviour
 
     private void Awake()
     {
-        _distanseBetweenTowers = _pathCreator.path.length / _towersOnRoad;
-
-        _spawnPosition = _pathCreator.path.GetPointAtDistance(_distanseBetweenTowers - 0.1f);
+        _distanseBetweenTowers = (_finishPosition.position.x - _startPosition.position.x) / _towersOnRoad;
+        
+        _spawnPosition = _pathCreator.path.GetPointAtDistance(_distanseBetweenTowers);
     }
 
     public List<Human> Build()
@@ -27,15 +29,20 @@ public class TowerBuilder : MonoBehaviour
 
         for (int i = 0; i < _towersOnRoad; i++)
         {
-
-            for (int j = 0; j < _humansInTower; j++)
-            {
+           for (int j = 0; j < _humansInTower; j++)
+           {
+                for (int k = 0; k < _humansInTower; k++)
+                {
+                    _spawnPosition.z = _pathCreator.path.GetClosestPointOnPath(_spawnPosition).z;
+                }
+                
                 Human newHuman = SpawnHuman();
-
+                
                 humans.Add(newHuman);
-
-                _spawnPosition = new Vector3(_spawnPosition.x, _spawnPosition.y + _human.transform.localScale.y * _spawnPositionYMultiplier, _spawnPosition.z);
-            }
+                
+                _spawnPosition.y += _human.transform.localScale.y * _spawnPositionYMultiplier;
+                _spawnPosition = new Vector3(_spawnPosition.x, _spawnPosition.y, _spawnPosition.z);
+           }
 
             _spawnPosition = new Vector3(_spawnPosition.x + _distanseBetweenTowers, 0, _spawnPosition.z);
         }
@@ -45,8 +52,8 @@ public class TowerBuilder : MonoBehaviour
 
     private Human SpawnHuman()
     {
-        Human newHuman = Instantiate(_human, _spawnPosition, Quaternion.identity, transform);
-
+        Human newHuman = Instantiate(_human, _spawnPosition, Quaternion.Euler(0, -90, 0), transform);
+        
         return newHuman;
     }
 }
